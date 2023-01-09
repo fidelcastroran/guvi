753428c50ad4cdd33321cca5c25a893ee365fe80
fidel castro <117566668+fidelcastroran@users.noreply.github.com>
2023-01-10 02:25:22 +0700

    Add files via upload

---
+++ day3/resume.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "FIDEL CASTRO.P",
+      "email": "fidelcastroprp@gmail.com",
+      "phone": +79962054966,
+      "degree": "M.S",
+      "location": {
+        "address": "No,13 arul oli street, nainarmandapam",
+        "postalCode": 605004,
+        "city": "Puducherry",
+        "state": "Puducherry",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/fidel-castro-rangan-48b49572/",
+          "github":"https://github.com/fidelcastroran"
+        }
+      ]
+    },
+    "work": [
+      {
+        "Organisation": "National Research Tomsk Polytechnic University",
+        "position": "Junior research fellow",
+        "startDate": "2018-08-28",
+        "endDate": "2023-04-15",
+        "summary": "Thermal hydraulics",
+      },
+    ],
+    "education": [
+      {
+        "University": "National Research Tomsk Polytechnic University",
+        "department": "Nuclear physics",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2018,
+        "gpa": 4.6,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "Intermediate",
+        "project": [
+          "calculator,ecommerce website"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil, Enlish, Russian, French",
+      }
+    ],
+    "interests": [
+      {
+        "name": "critical thinking,content creation,Problem solving",
+      }
+    ]
+  }
+  console.log(myResume);
