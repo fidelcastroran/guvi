df5ce0407eacf80685ccf7d035a4f6570d880b3d
fidel castro <117566668+fidelcastroran@users.noreply.github.com>
2023-01-11 02:12:33 +0700

    Update for in, foreach.js

--- day3/for in, foreach.js
+++ day3/for in, foreach.js
@@ -23,9 +23,36 @@ var res=[
       "title": "eum et est occaecati",
       
     }]
-    for( var key in res) {
-      console.log(  res[key].id, res[key].title )
-    }
-    for(var i=0; i<res.length;i++){
-      console.log( res[i].id, res[i].title)
-    }
+    // for in
+for (var key in res) {
+  console.log(res[key].id, res[key].title)
+// }  1 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+// 2 'qui est esse'
+// 3 'ea molestias quasi exercitationem repellat qui ipsa sit aut'
+// 4 'eum et est occaecati'
+ // for
+for (var i = 0; i < res.length; i++) {
+  console.log(res[i].id, res[i].title)
+}
+// 1 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+// 2 'qui est esse'
+// 3 'ea molestias quasi exercitationem repellat qui ipsa sit aut'
+// 4 'eum et est occaecati'
+// forEach
+res.forEach(function (key) { console.log(key.id, key.title); });
+// 1 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+// 2 'qui est esse'
+// 3 'ea molestias quasi exercitationem repellat qui ipsa sit aut'
+// 4 'eum et est occaecati'
+res.forEach((v) => console.log(v.id, v.title))
+// 1 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+// 2 'qui est esse'
+// 3 'ea molestias quasi exercitationem repellat qui ipsa sit aut'
+// 4 'eum et est occaecati'
+// for ..of
+let text = "";
+for (let x of res[key].id) {
+  text += x;
+}
+console.log(text);
+}
