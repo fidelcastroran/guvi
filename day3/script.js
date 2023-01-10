c980e84b7126721a2ed9561f843559ff0bf98c51
fidel castro <117566668+fidelcastroran@users.noreply.github.com>
2023-01-11 02:13:51 +0700

    Update script.js

--- day3/script.js
+++ day3/script.js
@@ -4,11 +4,15 @@ request.send();
 request.onload=function(){
     var json=JSON.parse(request.response);
     // console.log(json[0])
-
+// for in
 for( let key in json) 
     console.log( key, json[key].area, json[key].name )
   }
+//   for
 for(var i=0; i<json.length;i++){
     console.log(i, json[i].area, json[i].name)
 
 }
+// forEach
+json.forEach(function(key) { console.log( key.area,key.name) });
+json.forEach((v) =>  console.log(v.name,v.name))
