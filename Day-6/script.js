dc74fb0c595c3cbaf09c4337cb3e38a08aed9c7e
Fidel Castro <117566668+fidelcastroran@users.noreply.github.com>
2023-02-01 17:36:17 +0700

    Update script.js

--- Day-6/script.js
+++ Day-6/script.js
@@ -1,4 +1,5 @@
- 
+//  person class to hold all the details 
+
 var Person = function (firstname, lastname, age) {
     this.firstName = firstname;
     this.lastName = lastname;
@@ -16,7 +17,7 @@ var Person = function (firstname, lastname, age) {
   var helo= new Person("Manik", "P.A", 20);
   console.log(helo.toString());
   
-//   USing prototype
+//  second method for PERSOn class USing prototype 
   
   var Person = function (firstname, lastname, age) {
     this.firstName = firstname;
@@ -36,3 +37,23 @@ var Person = function (firstname, lastname, age) {
   var hel = new Person("MAnik", "Toman", 25);
   console.log(hel.toString());
 
+
+
+
+
+// UBER pRice calculation
+class pric{
+    constructor(d,p){
+        this.d = d // instance of this object
+        this.p = p
+    }
+
+    something(t) {
+        console.log(this.d+this.p+t);
+        // console.log(d+p);
+        // console.log(this.d);
+    }
+}
+
+var pri = new pric(10,20)
+console.log(pri)
